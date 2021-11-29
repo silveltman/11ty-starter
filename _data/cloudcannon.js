b4dfb4ba67af9acef35c98af7ad21fd22fff4614
Sil Veltman <silveltman@hotmail.com>
2021-11-29 18:51:58 +0100

    cloudcannon input settings added

--- _data/cloudcannon.js
+++ _data/cloudcannon.js
@@ -10,4 +10,21 @@ module.exports = {
     //   collections: ['myCollection']
     // }
   ],
+  _inputs: {
+    page_title: {
+      label: 'Pagina titel',
+      comment: 'Probeer je pagina te beschrijven in 50–60 karakters. Wordt weergegeven in Google en heeft invloed op je vindbaarheid'
+    },
+    page_description: {
+      label: 'Pagina beschrijving',
+      comment: 'Probeer je pagina te beschrijven in 120-50 karakters. Wordt weergegeven in Google en heeft invloed op je vindbaarheid'
+    },
+    image: {
+      label: 'Afbeelding upload'
+    },
+    alt: {
+      label: 'Afbeelding beschrijving',
+      comment: 'Omschrijf je afbeelding in maximaal 125 karakters'
+    }
+  }
 };
